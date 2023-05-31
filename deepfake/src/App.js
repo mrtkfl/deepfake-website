b7289782f631c6971cb66c004e85aeb7eae6704f
Sophie Schmitz <sophie.schmitz@hm.edu>
2023-05-31 10:50:17 +0200

    erster push

---
+++ deepfake/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <br />
+        <br />
+        <h2 className="welcome-text">
+          Welcome to
+        </h2>
+        <h2 className="celebrity-talk">
+          Celebrity Talk!
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
