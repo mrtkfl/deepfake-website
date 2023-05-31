52dff723ba021b454ae408d34f8cff2313625266
Sophie Schmitz <sophie.schmitz@hm.edu>
2023-05-31 11:56:11 +0200

    push

--- deepfake/src/App.js
+++ deepfake/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Startseite from "./Startseite"
+import Introductionpage from "./Introductionpage"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Startseite/>} />
+      <Route exact path="/Introductionpage" element={<Introductionpage/>} />
     </Routes>
     </BrowserRouter>
   );
