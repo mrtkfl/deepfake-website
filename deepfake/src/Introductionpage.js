126cad947dd834c7c6cba66aecb6608ceac87da3
Lea Zeilbeck <lea.zeilbeck@googlemail.com>
2023-06-13 16:11:31 +0200

    Audios

--- deepfake/src/Introductionpage.js
+++ deepfake/src/Introductionpage.js
@@ -4,19 +4,13 @@ import TinderCard from 'react-tinder-card'
 const db = [
   {
     name: 'Richard Hendricks',
-    url: './img/Bild.png'
-  },
-  {
-    name: 'Erlich Bachman',
-    url: './img/Bild.png'
-  },
-  {
-    name: 'Monica Hall',
-    url: './img/Bild.png'
+    url: './img/Bild.png',
+    audioo: './audio/my-audio-file.mp3'
   },
   {
     name: 'Jared Dunn',
-    url: './img/Bild.png'
+    url: './img/Bild.png',
+    audioo: './audio/Andrew.mp3'
   }
 ]
 
@@ -24,10 +18,19 @@ function Simple () {
   const characters = db
   const [lastDirection, setLastDirection] = useState()
 
-  const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete)
+  const swiped = (direction, character) => {
+    console.log('removing: ' + character.name)
     setLastDirection(direction)
+    setTimeout(() => {
+      const audio = new Audio(character.audioo);
+      audio.play();
+    }, 2000);
   }
+
+//   const swiped = (direction, nameToDelete) => {
+//     console.log('removing: ' + nameToDelete)
+//     setLastDirection(direction)
+//   }
 
   const outOfFrame = (name) => {
     console.log(name + ' left the screen!')
@@ -40,9 +43,18 @@ function Simple () {
       <h1>Celebrity Talk</h1>
       <div className='cardContainer'>
         {characters.map((character) =>
-          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)} preventSwipe={['up', 'down']}>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character)} 
+          onCardLeftScreen={() => outOfFrame(character.name)} preventSwipe={['up', 'down']}>
+
             <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
               <h3>{character.name}</h3>
+            </div>
+
+            <div>
+            <audio className='audios'>
+                <source src={character.audioo} type="audio/mpeg" />
+            </audio>
+
             </div>
           </TinderCard>
         )}
