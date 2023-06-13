60afa9c82632043d473b4e178328f723e3f7d309
RappingDwarf <daniel.wiesenfeld@gmx.de>
2023-06-13 15:15:28 +0200

    Card init

--- deepfake/src/Introductionpage.js
+++ deepfake/src/Introductionpage.js
@@ -1,12 +1,55 @@
-import React from "react";
-import './App.css';
+import React, { useState } from 'react'
+import TinderCard from 'react-tinder-card'
 
-function Introductionpage() {
-    return (
-        <div>
-            
-        </div>
-    );
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/Bild.png'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/Bild.png'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/Bild.png'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/Bild.png'
+  }
+]
+
+function Simple () {
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
+  return (
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>Celebrity Talk</h1>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+    </div>
+  )
 }
 
-export default Introductionpage;
+export default Simple
