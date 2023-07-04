037ca93d5da08668c5aa2ec2b4d3dce993140bc8
RappingDwarf <daniel.wiesenfeld@gmx.de>
2023-07-04 18:13:58 +0200

    Bug Fix und Fake Upload

--- deepfake/src/data.js
+++ deepfake/src/data.js
@@ -6,94 +6,192 @@ export const db = [
         text: ""
     },
     {
-        name: 'Karl Lauterbach',
-        url: './img/Karl.jpeg',
-        audioo: './audio/Karl.mp3',
+        name: 'Angela Merkel',
+        url: './img/Angela.jpeg',
+        audioo: './audio/Real/AMR-1.wav',
         art: true,
-        text: "Ich habe echt keinen Bock mehr."
+        text: "Ich habe heute früh vor meinen Kleiderschrank gestanden und irgendwie gedacht, rot geht nicht, knall grün geht nicht, blau war gestern."
     },
     {
-        name: 'Joe Biden',
-        url: './img/Joe.jpeg',
-        audioo: './audio/Joe.mp3',
+        name: 'Angela Merkel ',
+        url: './img/Angela.jpeg',
+        audioo: './audio/Real/AMR-2.wav',
         art: true,
-        text: "Ich habe echt keinen Bock mehr."
+        text: "Es ist bekannt, dass der Russische Präsident gerne deutsches Bier trinkt."
+    },
+    {
+        name: 'Angela Merkel  ',
+        url: './img/Angela.jpeg',
+        audioo: './audio/Fake/AMF-1.wav',
+        art: false,
+        text: "Es braucht eine alternative für Deutschland."
+    },
+    {
+        name: 'Angela Merkel   ',
+        url: './img/Angela.jpeg',
+        audioo: './audio/Fake/AMF-2.wav',
+        art: false,
+        text: "Ich mag Bier, in der tat nicht sehr gerne."
+    },
+    {
+        name: 'Angela Merkel    ',
+        url: './img/Angela.jpeg',
+        audioo: './audio/Fake/AMF-3.wav',
+        art: false,
+        text: "Döner sollte nicht mehr als 4 Euro kosten."
+    },
+    {
+        name: 'Bernd das Brot',
+        url: './img/Bernd.png',
+        audioo: './audio/Real/BBR-1.wav',
+        art: true,
+        text: "Mach lieber mal deinen Job!"
+    },
+    {
+        name: 'Bernd das Brot ',
+        url: './img/Bernd.png',
+        audioo: './audio/Real/BBR-2.wav',
+        art: true,
+        text: "Wir sind Brot, wiederstand ist zwecklos."
+    },
+    {
+        name: 'Bernd das Brot  ',
+        url: './img/Bernd.png',
+        audioo: './audio/Real/BBR-3.wav',
+        art: true,
+        text: "Hört auf hier anzurufen."
+    },
+    {
+        name: 'Bernd das Brot   ',
+        url: './img/Bernd.png',
+        audioo: './audio/Fake/BBF-1.wav',
+        art: false,
+        text: "Es gibt nichts, was ich so hasse, wie Toastbrot."
+    },
+    {
+        name: 'Bernd das Brot    ',
+        url: './img/Bernd.png',
+        audioo: './audio/Fake/BBF-2.wav',
+        art: false,
+        text: "Ich bin eigentlich, ein Baguette."
+    },
+    {
+        name: 'Bernd das Brot     ',
+        url: './img/Bernd.png',
+        audioo: './audio/Fake/BBF-3.wav',
+        art: false,
+        text: "Exestieren, ist scheiße."
     },
     {
         name: 'Donald Trump',
         url: './img/Donald.jpeg',
-        audioo: './audio/Real/DTR-1.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Angela Merkel',
-        url: './img/Angela.jpeg',
-        audioo: './audio/Angela.mp3',
-        art: false,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Karl Lauterbach2',
-        url: './img/Karl.jpeg',
-        audioo: './audio/Karl.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Joe Biden2',
-        url: './img/Joe.jpeg',
-        audioo: './audio/Joe.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Donald Trump2',
-        url: './img/Donald.jpeg',
-        audioo: './audio/Real/DTR-1.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Angela Merkel2',
-        url: './img/Angela.jpeg',
-        audioo: './audio/Angela.mp3',
-        art: false,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Karl Lauterbach3',
-        url: './img/Karl.jpeg',
-        audioo: './audio/Karl.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Joe Biden3',
-        url: './img/Joe.jpeg',
-        audioo: './audio/Joe.mp3',
-        art: true,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Donald Trump3',
-        url: './img/Donald.jpeg',
         audioo: './audio/Real/DTR-1.wav',
         art: true,
-        text: "Ich habe echt keinen Bock mehr."
+        text: "I could be the most popular person in Europe."
     },
     {
-        name: 'Angela Merkel3',
-        url: './img/Angela.jpeg',
-        audioo: './audio/Angela.mp3',
-        art: false,
-        text: "Ich habe echt keinen Bock mehr."
-    },
-    {
-        name: 'Bernd das Brot3',
-        url: './img/Bernd.png',
-        audioo: './audio/Bernd.mp3',
+        name: 'Donald Trump ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Real/DTR-2.wav',
         art: true,
-        text: "Es gibt ein Brot das ich hasse und das ist Toastbrot."
+        text: "I am the chosen one, somebody had to do it."
+    },
+    {
+        name: 'Donald Trump  ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Real/DTR-3.wav',
+        art: true,
+        text: "I have unbelivable relationships, with all the foreign leaders. They like me, I like them."
+    },
+    {
+        name: 'Donald Trump   ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Real/DTR-4.wav',
+        art: true,
+        text: "Where I could stand in the middle of fith avenue and shoot somebody and I woulnd't loose any voters. Okay?"
+    },
+    {
+        name: 'Donald Trump    ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Fake/DTF-1.wav',
+        art: false,
+        text: "The only thing, Joe Biden loves, is Ice Cream."
+    },
+    {
+        name: 'Donald Trump     ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Fake/DTF-2.wav',
+        art: false,
+        text: "The Doner Kebab has become, outstandingly expensive over the last year."
+    },
+    {
+        name: 'Donald Trump      ',
+        url: './img/Donald.jpeg',
+        audioo: './audio/Fake/DTF-3.wav',
+        art: false,
+        text: "I promised the American people I would build a wall, but since the elections was rigged I have to do it in Minecraft."
+    },
+    {
+        name: 'Karl Lauterbach',
+        url: './img/Karl.jpeg',
+        audioo: './audio/Real/KLR-1.wav',
+        art: true,
+        text: "Ich freue mich heute hier zu sein."
+    },
+    {
+        name: 'Karl Lauterbach ',
+        url: './img/Karl.jpeg',
+        audioo: './audio/Real/KLR-2.wav',
+        art: true,
+        text: "Ich neide Jens Spahn nichts."
+    },
+    {
+        name: 'Karl Lauterbach  ',
+        url: './img/Karl.jpeg',
+        audioo: './audio/Fake/KLF-1.wav',
+        art: false,
+        text: "Corona ist ein Virus aus China."
+    },
+    {
+        name: 'Karl Lauterbach   ',
+        url: './img/Karl.jpeg',
+        audioo: './audio/Fake/KLF-2.wav',
+        art: false,
+        text: "Ich kann mit einer Maske nicht Atmen."
+    },
+    {
+        name: 'Morgan Freeman',
+        url: './img/Morgan.jpeg',
+        audioo: './audio/Real/MFR-1.wav',
+        art: true,
+        text: "The lower your voice is, the better you sound."
+    },
+    {
+        name: 'Morgan Freeman ',
+        url: './img/Morgan.jpeg',
+        audioo: './audio/Real/MFR-2.wav',
+        art: true,
+        text: "Hmmmm, just listen to that rich molasis."
+    },
+    {
+        name: 'Morgan Freeman  ',
+        url: './img/Morgan.jpeg',
+        audioo: './audio/Fake/MFF-1.wav',
+        art: false,
+        text: "I think the police is useless."
+    },
+    {
+        name: 'Morgan Freeman   ',
+        url: './img/Morgan.jpeg',
+        audioo: './audio/Fake/MFF-2.wav',
+        art: false,
+        text: "I love a good Kebab."
+    },
+    {
+        name: 'Morgan Freeman    ',
+        url: './img/Morgan.jpeg',
+        audioo: './audio/Fake/MFF-3.wav',
+        art: true,
+        text: "I don't like, Jennifer Lawrence."
     }
 ]
