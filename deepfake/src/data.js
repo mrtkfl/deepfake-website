3ec430f10d5e8faa20218a3bc1d86233b220c458
RappingDwarf <daniel.wiesenfeld@gmx.de>
2023-07-05 09:42:56 +0200

    Audio Add

--- deepfake/src/data.js
+++ deepfake/src/data.js
@@ -7,35 +7,35 @@ export const db = [
     },
     {
         name: 'Angela Merkel',
-        url: './img/Angela.jpeg',
+        url: './img/Angela.png',
         audioo: './audio/Real/AMR-1.wav',
         art: true,
         text: "Ich habe heute früh vor meinen Kleiderschrank gestanden und irgendwie gedacht, rot geht nicht, knall grün geht nicht, blau war gestern."
     },
     {
         name: 'Angela Merkel ',
-        url: './img/Angela.jpeg',
+        url: './img/Angela.png',
         audioo: './audio/Real/AMR-2.wav',
         art: true,
         text: "Es ist bekannt, dass der Russische Präsident gerne deutsches Bier trinkt."
     },
     {
         name: 'Angela Merkel  ',
-        url: './img/Angela.jpeg',
+        url: './img/Angela.png',
         audioo: './audio/Fake/AMF-1.wav',
         art: false,
         text: "Es braucht eine alternative für Deutschland."
     },
     {
         name: 'Angela Merkel   ',
-        url: './img/Angela.jpeg',
+        url: './img/Angela.png',
         audioo: './audio/Fake/AMF-2.wav',
         art: false,
         text: "Ich mag Bier, in der tat nicht sehr gerne."
     },
     {
         name: 'Angela Merkel    ',
-        url: './img/Angela.jpeg',
+        url: './img/Angela.png',
         audioo: './audio/Fake/AMF-3.wav',
         art: false,
         text: "Döner sollte nicht mehr als 4 Euro kosten."
@@ -84,114 +84,163 @@ export const db = [
     },
     {
         name: 'Donald Trump',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Real/DTR-1.wav',
         art: true,
         text: "I could be the most popular person in Europe."
     },
     {
         name: 'Donald Trump ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Real/DTR-2.wav',
         art: true,
         text: "I am the chosen one, somebody had to do it."
     },
     {
         name: 'Donald Trump  ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Real/DTR-3.wav',
         art: true,
         text: "I have unbelivable relationships, with all the foreign leaders. They like me, I like them."
     },
     {
         name: 'Donald Trump   ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Real/DTR-4.wav',
         art: true,
         text: "Where I could stand in the middle of fith avenue and shoot somebody and I woulnd't loose any voters. Okay?"
     },
     {
         name: 'Donald Trump    ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Fake/DTF-1.wav',
         art: false,
         text: "The only thing, Joe Biden loves, is Ice Cream."
     },
     {
         name: 'Donald Trump     ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Fake/DTF-2.wav',
         art: false,
         text: "The Doner Kebab has become, outstandingly expensive over the last year."
     },
     {
         name: 'Donald Trump      ',
-        url: './img/Donald.jpeg',
+        url: './img/Donald.png',
         audioo: './audio/Fake/DTF-3.wav',
         art: false,
         text: "I promised the American people I would build a wall, but since the elections was rigged I have to do it in Minecraft."
     },
     {
         name: 'Karl Lauterbach',
-        url: './img/Karl.jpeg',
+        url: './img/Karl.png',
         audioo: './audio/Real/KLR-1.wav',
         art: true,
         text: "Ich freue mich heute hier zu sein."
     },
     {
         name: 'Karl Lauterbach ',
-        url: './img/Karl.jpeg',
+        url: './img/Karl.png',
         audioo: './audio/Real/KLR-2.wav',
         art: true,
         text: "Ich neide Jens Spahn nichts."
     },
     {
         name: 'Karl Lauterbach  ',
-        url: './img/Karl.jpeg',
+        url: './img/Karl.png',
         audioo: './audio/Fake/KLF-1.wav',
         art: false,
         text: "Corona ist ein Virus aus China."
     },
     {
         name: 'Karl Lauterbach   ',
-        url: './img/Karl.jpeg',
+        url: './img/Karl.png',
         audioo: './audio/Fake/KLF-2.wav',
         art: false,
         text: "Ich kann mit einer Maske nicht Atmen."
     },
     {
         name: 'Morgan Freeman',
-        url: './img/Morgan.jpeg',
+        url: './img/Morgan.png',
         audioo: './audio/Real/MFR-1.wav',
         art: true,
         text: "The lower your voice is, the better you sound."
     },
     {
         name: 'Morgan Freeman ',
-        url: './img/Morgan.jpeg',
+        url: './img/Morgan.png',
         audioo: './audio/Real/MFR-2.wav',
         art: true,
         text: "Hmmmm, just listen to that rich molasis."
     },
     {
         name: 'Morgan Freeman  ',
-        url: './img/Morgan.jpeg',
+        url: './img/Morgan.png',
         audioo: './audio/Fake/MFF-1.wav',
         art: false,
         text: "I think the police is useless."
     },
     {
         name: 'Morgan Freeman   ',
-        url: './img/Morgan.jpeg',
+        url: './img/Morgan.png',
         audioo: './audio/Fake/MFF-2.wav',
         art: false,
         text: "I love a good Kebab."
     },
     {
         name: 'Morgan Freeman    ',
-        url: './img/Morgan.jpeg',
+        url: './img/Morgan.png',
         audioo: './audio/Fake/MFF-3.wav',
         art: true,
         text: "I don't like, Jennifer Lawrence."
-    }
+    },
+    {
+        name: 'Joe Biden',
+        url: './img/Joe.png',
+        audioo: './audio/Real/JBR-1.wav',
+        art: true,
+        text: "I'd be fun watching them take on each other."
+    },
+    {
+        name: 'Joe Biden ',
+        url: './img/Joe.png',
+        audioo: './audio/Real/JBR-2.wav',
+        art: true,
+        text: "Same Stable geniuses said, the biggest problem we had in the revolutionary war, is we didn't have enough Airports!"
+    },
+    {
+        name: 'Joe Biden  ',
+        url: './img/Joe.png',
+        audioo: './audio/Real/JBR-3.wav',
+        art: true,
+        text: "What a stupid son of a bitch."
+    },
+    {
+        name: 'Joe Biden   ',
+        url: './img/Joe.png',
+        audioo: './audio/Fake/JBF-1.wav',
+        art: false,
+        text: "Wow Obama, bombing houses again. I though you've changed since Yemen."
+    },
+    {
+        name: 'Joe Biden    ',
+        url: './img/Joe.png',
+        audioo: './audio/Fake/JBF-2.wav',
+        art: false,
+        text: "I've to admit, I actually love Donald Trump."
+    },
+    {
+        name: 'Joe Biden     ',
+        url: './img/Joe.png',
+        audioo: './audio/Fake/JBF-3.wav',
+        art: false,
+        text: "Vanilla Ice Cream is the fucking best."
+    },
+    {
+        name: 'Barack Obama',
+        url: './img/Obama.png',
+        audioo: './audio/Fake/JBF-3.wav',
+        art: false,
+        text: "Vanilla Ice Cream is the fucking best."
+    },
 ]
